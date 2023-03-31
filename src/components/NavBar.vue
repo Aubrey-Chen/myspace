1442e2d5feae6ca0ded2edc47948ed9fde2ebf60
Aubrey Chen <aubrey_chen@foxmail.com>
2023-03-31 17:01:48 +0800

    主页面路由跳转——每打开一次页面就会刷新一遍，刷新表示从服务器重新请求一遍数据，但这不是前端渲染

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,28 +1,28 @@
 <template>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container ">
-    <a class="navbar-brand" href="#">MySpace</a>
+    <a class="navbar-brand" href="/">MySpace</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">首页</a>
+          <a class="nav-link" href="/">首页</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">好友列表</a>
+          <a class="nav-link" href="/userlist">好友列表</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">用户动态</a>
+          <a class="nav-link" href="/userprofile">用户动态</a>
         </li>
       </ul>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">登录</a>
+          <a class="nav-link" href="/login">登录</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">注册</a>
+          <a class="nav-link" href="/register">注册</a>
         </li>
       </ul>
     </div>
