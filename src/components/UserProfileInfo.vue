4d2ebbb7e77b04a1475a0be1be05ca552a811850
Aubrey Chen <aubrey_chen@foxmail.com>
2023-04-03 11:32:22 +0800

    实现用户动态页面里的第一个组件用户信息（组件的动态暂未实现）

---
+++ src/components/UserProfileInfo.vue
@@ -0,0 +1,46 @@
+<template>
+    <!-- 用一个卡片将“用户信息”括起来 -->
+    <div class="card">
+        <div class="card-body">
+           <div class="row">
+                <div class="col-3">
+                    <img  class="img-fluid" src="https://cdn.acwing.com/media/user/profile/photo/129450_lg_33f926f237.jpg">
+                </div>
+                <div class="vol-9">
+                    <div class="username">Scribouillard</div>
+                    <div class="fans">粉丝：250</div>
+                    <!-- 控制图标大小的属性：btn-sm（小图标），btn-lg（大图标） -->
+                    <button type="button" class="btn btn-secondary btn-sm">🔔 关注</button>
+                </div>
+            </div> 
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'UserProfileInfo'
+}
+</script>
+
+<style scoped>
+/* 将图片样式设置为圆形 */
+img{
+    border-radius: 50%;
+}
+
+.username {
+    font-weight: bold;
+}
+
+.fans {
+    font-size: 12px;
+    color: gra;
+}
+
+button {
+    padding: 2px 4px;
+    font-size: 12px;
+}
+</style>
--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,18 +1,25 @@
 <template>
   <!-- 将“用户动态”渲染到ContentBase里面 -->
   <ContentBase>
-    用户动态
+    <div class="row">
+      <div class="col-3">
+        <UserProfileInfo/>
+      </div>
+      <div class="col-9">帖子列表</div>
+    </div>
   </ContentBase>
 </template>
 
 <script>
-import ContentBase from '../components/ContentBase'
+import ContentBase from '../components/ContentBase'; 
+import UserProfileInfo from '../components/UserProfileInfo.vue';
 
 export default {
   name: 'UserProfile', 
   // component表示在template区域会用到哪些其他的组件，把所有用到的组件全部放到component里面。
   components: {
-    ContentBase
+    ContentBase,
+    UserProfileInfo
   }
 }
 </script>
