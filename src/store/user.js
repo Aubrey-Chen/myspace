05730a18810e2393c6ecfa992048f03792ca3375
Aubrey Chen <aubrey_chen@foxmail.com>
2023-04-19 18:38:53 +0800

    在终端执行`npm i jwt-decode`命令来安装JWT解码包；实现JWT的验证和获取access访问令牌、refresh刷新令牌：根据user_id获取用户信息以及jwt验证和调用已定义的mutations里的API。将resp解构再调用对应的回调函数。

--- src/store/user.js
+++ src/store/user.js
@@ -1,35 +1,80 @@
 import $ from 'jquery';
+import jwt_decode from 'jwt-decode';
 
 const userModule = {
-    state: {
-      id: "", 
-      username: "", 
-      photo: "", 
-      folllowerCount: 0, 
-    }, 
-     getters: {
+  state: {
+    id: "", 
+    username: "", 
+    photo: "", 
+    folllowerCount: 0, 
+    // 访问令牌
+    access: "", 
+    // 刷新令牌
+    refresh: "", 
+    is_login: false, 
+  }, 
+    getters: {
 
-    },
-    mutations: {
- 
-    },
-    actions: {
-      login(context, data) {
-        $.ajax({
-          url: "https://app165.acapp.acwing.com.cn/api/token/", 
-          type: "POST", 
-          data: {
-            username: data.username, 
-            password: data.password, 
-          }, 
-          success: function(response) {
-            console.log(response);
-          }, 
-        });
-      }, 
-    },
-    modules: {
-    }
+  },
+  mutations: {
+    updateUser(state, user) {
+      state.id = user.id;
+      state.username = user.username;
+      state.photo = user.photo;
+      state.folllowerCount = user.folllowerCount;
+      state.access = user.access;
+      state.refresh = user.refresh;
+      state.is_login = user.is_login;
+    }, 
+  },
+  actions: {
+    login(context, data) {
+      $.ajax({
+        url: "https://app165.acapp.acwing.com.cn/api/token/", 
+        type: "POST", 
+        data: {
+          username: data.username, 
+          password: data.password, 
+        }, 
+        success(resp) {
+          const {access, refresh} = resp;
+          // jwt解码
+          const access_obj = jwt_decode(access);
+          // 根据user_id获取用户信息
+          $.ajax({
+            url: "https://app165.acapp.acwing.com.cn/myspace/getinfo/", 
+            type: "GET", 
+            data: {
+              // data里面是需要传入的参数
+              user_id: access_obj.user_id, 
+            }, 
+            // jwt验证
+            headers: {
+              // 注意：Bearer后有一个空格符！
+              'Authorization': "Bearer " + access, 
+            }, 
+            success(resp) {
+              // 调用已定义的mutations里的API
+              context.commit("updateUser", {
+                // ...作用是把数组或类数组对象展开成一系列用逗号隔开的值
+                ...resp, // 将resp解构
+                access: access, 
+                refresh: refresh, 
+                is_login: true, 
+              });
+              // 调用回调函数
+              data.success();
+            }, 
+          }); 
+        }, 
+        error() {
+          data.error();
+        }
+      });
+    }, 
+  },
+  modules: {
+  }
 }; 
 
 export default userModule;
